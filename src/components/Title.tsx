e243601b631b797bb2b3a36043281ddddabbac2a
Nhung Nguyen <iminb2b@gmail.com>
2024-11-03 07:45:17 -0800

    add nanoid and Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,60 @@
+import { NodeData } from "@/types/nodeType";
+import { css } from "@emotion/react";
+import {
+  FC,
+  FormEventHandler,
+  KeyboardEventHandler,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
+import { nanoid } from "nanoid";
+
+type TitleProps = {
+  title: string;
+  changPageTitle: (title: string) => void;
+  addNode: (params: { node: NodeData; index: number }) => void;
+};
+
+const container = css``;
+const titleText = css``;
+
+const Title: FC<TitleProps> = ({ title, changPageTitle, addNode }) => {
+  const headerRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const isFocused = document.activeElement === headerRef.current;
+
+    if (!isFocused && headerRef.current) {
+      headerRef.current.textContent = title;
+    }
+  }, []);
+
+  const onInput: FormEventHandler<HTMLHeadingElement> = useCallback((event) => {
+    changPageTitle(event.currentTarget.textContent || "");
+  }, []);
+
+  const onKeyDown: KeyboardEventHandler<HTMLHeadingElement> = useCallback(
+    (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        addNode({ node: { type: "text", id: nanoid(), value: "" }, index: 0 });
+      }
+    },
+    [],
+  );
+
+  return (
+    <div>
+      <h1
+        css={titleText}
+        contentEditable
+        suppressContentEditableWarning
+        onInput={onInput}
+        onKeyDown={onKeyDown}
+      ></h1>
+    </div>
+  );
+};
+
+export default Title;
--- src/types/nodeType.ts
+++ src/types/nodeType.ts
@@ -6,3 +6,17 @@ export type NodeType =
   | "heading1"
   | "heading2"
   | "heading3";
+
+export type NodeData = {
+  id: string;
+  type: NodeType;
+  value: string;
+};
+
+export type Page = {
+  id: string;
+  slug: string;
+  title: string;
+  nodes: NodeData[];
+  cover: string;
+};
